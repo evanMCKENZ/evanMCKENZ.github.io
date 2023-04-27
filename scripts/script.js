d056a49b469b7b4634086859b0cbd8c495d8a991
Evan McKenzie <90295868+evanMCKENZ@users.noreply.github.com>
2023-04-26 21:00:12 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const header = document.querySelector('.navbar');
 console.log(header)
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=100) {
+    if(top >=0) {
         header.classList.add('navbarDark');
     }
     else {
